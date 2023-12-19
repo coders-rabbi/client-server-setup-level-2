5bae6a67bf9aa60c7b43da1651a6c982c2bd89e1
coders-rabbi <pcrabbi2020@gmail.com>
2023-12-19 14:38:14 +0600

    done create student feature

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     await mongoose.connect(config.databse_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
