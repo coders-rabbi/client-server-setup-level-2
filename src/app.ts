5a821ec6397a05c51ff83e04d59fdfac2d68786a
coders-rabbi <pcrabbi2020@gmail.com>
2023-12-25 23:14:55 +0600

    seperate route created done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalerErrorHandeler from './app/middlewares/globalErrorHangler';
 import notFound from './app/middlewares/notfound';
+import router from './app/routes/routers';
 const app: Application = express();
 
 //parser
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
   // const a = 20;
